97ccdc527aab5871e8974c1d8efc1b508ad90989
mori-inj <leokim1022@snu.ac.kr>
2016-09-16 21:05:18 +0900

    xlsx parser added

--- test/Program.cs
+++ test/Program.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +16,69 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><w:settings xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:m=\"http://schemas.openxmlformats.org/officeDocument/2006/math\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:w10=\"urn:schemas-microsoft-com:office:word\" xmlns:w=\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\" xmlns:sl=\"http://schemas.openxmlformats.org/schemaLibrary/2006/main\"><w:zoom w:percent=\"125\"/><w:bordersDoNotSurroundHeader/><w:bordersDoNotSurroundFooter/><w:proofState w:spelling=\"clean\" w:grammar=\"clean\"/><w:defaultTabStop w:val=\"800\"/><w:displayHorizontalDrawingGridEvery w:val=\"0\"/><w:displayVerticalDrawingGridEvery w:val=\"2\"/><w:noPunctuationKerning/><w:characterSpacingControl w:val=\"doNotCompress\"/><w:compat><w:spaceForUL/><w:balanceSingleByteDoubleByteWidth/><w:doNotLeaveBackslashAlone/><w:ulTrailSpace/><w:doNotExpandShiftReturn/><w:adjustLineHeightInTable/><w:useFELayout/></w:compat><w:rsids><w:rsidRoot w:val=\"0020395F\"/><w:rsid w:val=\"00004729\"/><w:rsid w:val=\"001127CB\"/><w:rsid w:val=\"0019669C\"/><w:rsid w:val=\"0020395F\"/><w:rsid w:val=\"0064385F\"/><w:rsid w:val=\"007E4BD3\"/><w:rsid w:val=\"00BD0195\"/><w:rsid w:val=\"00E84F1C\"/></w:rsids><m:mathPr><m:mathFont m:val=\"Cambria Math\"/><m:brkBin m:val=\"before\"/><m:brkBinSub m:val=\"--\"/><m:smallFrac m:val=\"off\"/><m:dispDef/><m:lMargin m:val=\"0\"/><m:rMargin m:val=\"0\"/><m:defJc m:val=\"centerGroup\"/><m:wrapIndent m:val=\"1440\"/><m:intLim m:val=\"subSup\"/><m:naryLim m:val=\"undOvr\"/></m:mathPr><w:themeFontLang w:val=\"en-US\" w:eastAsia=\"ko-KR\"/><w:clrSchemeMapping w:bg1=\"light1\" w:t1=\"dark1\" w:bg2=\"light2\" w:t2=\"dark2\" w:accent1=\"accent1\" w:accent2=\"accent2\" w:accent3=\"accent3\" w:accent4=\"accent4\" w:accent5=\"accent5\" w:accent6=\"accent6\" w:hyperlink=\"hyperlink\" w:followedHyperlink=\"followedHyperlink\"/><w:shapeDefaults><o:shapedefaults v:ext=\"edit\" spidmax=\"2050\"/><o:shapelayout v:ext=\"edit\"><o:idmap v:ext=\"edit\" data=\"1\"/></o:shapelayout></w:shapeDefaults><w:decimalSymbol w:val=\".\"/><w:listSeparator w:val=\",\"/></w:settings>";
-            Console.WriteLine(PrintXML(xml));
+            //string xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><w:settings xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:m=\"http://schemas.openxmlformats.org/officeDocument/2006/math\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:w10=\"urn:schemas-microsoft-com:office:word\" xmlns:w=\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\" xmlns:sl=\"http://schemas.openxmlformats.org/schemaLibrary/2006/main\"><w:zoom w:percent=\"125\"/><w:bordersDoNotSurroundHeader/><w:bordersDoNotSurroundFooter/><w:proofState w:spelling=\"clean\" w:grammar=\"clean\"/><w:defaultTabStop w:val=\"800\"/><w:displayHorizontalDrawingGridEvery w:val=\"0\"/><w:displayVerticalDrawingGridEvery w:val=\"2\"/><w:noPunctuationKerning/><w:characterSpacingControl w:val=\"doNotCompress\"/><w:compat><w:spaceForUL/><w:balanceSingleByteDoubleByteWidth/><w:doNotLeaveBackslashAlone/><w:ulTrailSpace/><w:doNotExpandShiftReturn/><w:adjustLineHeightInTable/><w:useFELayout/></w:compat><w:rsids><w:rsidRoot w:val=\"0020395F\"/><w:rsid w:val=\"00004729\"/><w:rsid w:val=\"001127CB\"/><w:rsid w:val=\"0019669C\"/><w:rsid w:val=\"0020395F\"/><w:rsid w:val=\"0064385F\"/><w:rsid w:val=\"007E4BD3\"/><w:rsid w:val=\"00BD0195\"/><w:rsid w:val=\"00E84F1C\"/></w:rsids><m:mathPr><m:mathFont m:val=\"Cambria Math\"/><m:brkBin m:val=\"before\"/><m:brkBinSub m:val=\"--\"/><m:smallFrac m:val=\"off\"/><m:dispDef/><m:lMargin m:val=\"0\"/><m:rMargin m:val=\"0\"/><m:defJc m:val=\"centerGroup\"/><m:wrapIndent m:val=\"1440\"/><m:intLim m:val=\"subSup\"/><m:naryLim m:val=\"undOvr\"/></m:mathPr><w:themeFontLang w:val=\"en-US\" w:eastAsia=\"ko-KR\"/><w:clrSchemeMapping w:bg1=\"light1\" w:t1=\"dark1\" w:bg2=\"light2\" w:t2=\"dark2\" w:accent1=\"accent1\" w:accent2=\"accent2\" w:accent3=\"accent3\" w:accent4=\"accent4\" w:accent5=\"accent5\" w:accent6=\"accent6\" w:hyperlink=\"hyperlink\" w:followedHyperlink=\"followedHyperlink\"/><w:shapeDefaults><o:shapedefaults v:ext=\"edit\" spidmax=\"2050\"/><o:shapelayout v:ext=\"edit\"><o:idmap v:ext=\"edit\" data=\"1\"/></o:shapelayout></w:shapeDefaults><w:decimalSymbol w:val=\".\"/><w:listSeparator w:val=\",\"/></w:settings>";
+            //Console.WriteLine(PrintXML(xml));
+
+            string fileName = @"C:\Users\Doos\Desktop\aaaa.xlsx";
+            ParseXlsx(fileName);
+            
         }
 
+
+        public static void ParseXlsx(string fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (SpreadsheetDocument doc = SpreadsheetDocument.Open(fs, false))
+                {
+                    WorkbookPart workbookPart = doc.WorkbookPart;
+                    SharedStringTablePart sstpart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+                    SharedStringTable sst = sstpart.SharedStringTable;
+
+                    WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                    Worksheet sheet = worksheetPart.Worksheet;
+
+                    var cells = sheet.Descendants<Cell>();
+                    var rows = sheet.Descendants<Row>();
+
+                    Console.WriteLine("Row count = {0}", rows.LongCount());
+                    Console.WriteLine("Cell count = {0}", cells.LongCount());
+
+                    // One way: go through each cell in the sheet
+                    foreach (Cell cell in cells)
+                    {
+                        if ((cell.DataType != null) && (cell.DataType == CellValues.SharedString))
+                        {
+                            int ssid = int.Parse(cell.CellValue.Text);
+                            string str = sst.ChildElements[ssid].InnerText;
+                            Console.WriteLine("Shared string {0}: {1}", ssid, str);
+                        }
+                        else if (cell.CellValue != null)
+                        {
+                            Console.WriteLine("Cell contents: {0}", cell.CellValue.Text);
+                        }
+                    }
+
+                    // Or... via each row
+                    foreach (Row row in rows)
+                    {
+                        foreach (Cell c in row.Elements<Cell>())
+                        {
+                            if ((c.DataType != null) && (c.DataType == CellValues.SharedString))
+                            {
+                                int ssid = int.Parse(c.CellValue.Text);
+                                string str = sst.ChildElements[ssid].InnerText;
+                                Console.WriteLine("Shared string {0}: {1}", ssid, str);
+                            }
+                            else if (c.CellValue != null)
+                            {
+                                Console.WriteLine("Cell contents: {0}", c.CellValue.Text);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
         public static String PrintXML(String XML)
         {
